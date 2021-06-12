89fd47782b32f8f2bee5fdff0430a33715cfafd9
BigGuyAlex <84516641+BigGuyAlex@users.noreply.github.com>
2021-06-12 02:02:48 +0000

    ktctxrlxfcgluv

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+document.getElementById('button').addEventListener('click', multiply)
 let answer = 0
 
 function multiply () {
-  const number1 = parseInt(document.getElementById('number1').value)
-  const number2 = parseInt(document.getElementById('number2').value)
-  for (let i = 0; i < 1; i++) {
-    answer += 2
+  const #1 = parseInt(document.getElementById('numberone').value)
+  const #2 = parseInt(document.getElementById('numbertwo').value)
+  for (let i = 0; i < numberone; i++) {
+    answer += numbertwo
   }
-  
+  document.getElementById('answer').innerHTML = answer
+}
