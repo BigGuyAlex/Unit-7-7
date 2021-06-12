36f13640e058044ba5c9bdbeb6ebb24e1d447871
BigGuyAlex <84516641+BigGuyAlex@users.noreply.github.com>
2021-06-12 02:07:06 +0000

    HAHhebyefuwo

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.getElementById('button').addEventListener('click', multiply)
 let answer = 0
 
 function multiply () {
-  const #1 = parseInt(document.getElementById('numberone').value)
-  const #2 = parseInt(document.getElementById('numbertwo').value)
+  const numberone = parseInt(document.getElementById('numberone').value)
+  const numbertwo = parseInt(document.getElementById('numbertwo').value)
   for (let i = 0; i < numberone; i++) {
     answer += numbertwo
   }
