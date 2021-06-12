11eeb60ad56182a2f454e833d2c5d7c6cb982496
BigGuyAlex <84516641+BigGuyAlex@users.noreply.github.com>
2021-06-12 01:38:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let answer = 0
+
+function multiply () {
+  const number1 = parseInt(document.getElementById('number1').value)
+  const number2 = parseInt(document.getElementById('number2').value)
+  for (let i = 0; i < 1; i++) {
+    answer += 2
+  }
+  
